51c5117531e53aa54982bfdd8fb581d5531c76c2
Jamie Hewitt <jamie.hewitt15@gmail.com>
2019-12-10 13:45:50 +0300

    updates to index page layout

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -4,7 +4,7 @@ module.exports.homepage = function(req, res){
                                 title: "Latest Updates",
                                 strapline: "Welcome to the Solidaridad Intranet - Keep up to date with the latest news from the network."
                             },
-                            fullName: "Test",
+                            fullName: "Jamie Hewitt",
                             description: "Developer of web applications, JavaScript, PHP, Java, Python, Ruby, Java, Node.js, etc.",
                             followers: 5234,
                             following: 3960,
@@ -15,7 +15,8 @@ module.exports.homepage = function(req, res){
                                     text: "Hello World!! This is the text of my first post. Thank you for reading it!",
                                     time: "Monday 10am",
                                     user: "Rachel Wanyoike",
-                                    tags: ["East Africa", "Coffee", "Sustainablility"]
+                                    profilePic: "/images/user-profile.png",
+                                    tags: ["East Africa", "Kenya", "Coffee", "Sustainablility"]
                                 },
                                 {
                                     title: "Excited to announce my second post",
@@ -23,6 +24,7 @@ module.exports.homepage = function(req, res){
                                     text: "Hello World!! This is the text of my second post. Thank you for reading it!",
                                     time: "Wednesday 5pm",
                                     user: "Jamie Hewitt",
+                                    profilePic: "/images/user-profile.png",
                                     tags: ["Asia", "Tea", "Resiliance"]
                                 
                                 }
@@ -50,7 +52,19 @@ module.exports.qa = function(req, res){
 }
 
 module.exports.chat = function(req, res){
-    res.render('chat', { title: 'chat' });
+    res.render('chat', {    title: 'chat',
+                            groups: ["Coffee Team", "Kenya Team", "Management", "IT Issues"],
+                            recentContacts: ["John Johnson", "Sally Saunders", "Matt Mathews", "Rachel Richards"],
+                            names: ["Nathaneal Down","Natalya Deck","Gustav Purpleson","Alan Fresco","Sarah Purpleson","Juliet Fresco","Valentino Morose","Ruby Von Rails","Hilary Ouse","Richard Tea","Nigel Hollandaise","Gideon Guernsey-Marmaduke","Fig Nelson","Hugh Saturation","Gordon Norman","Abraham Pigeon","Jason Response","Piff Jenkins","Maggie Morose","Rachel Rails","Hilary Hornet"],
+                            messages: {
+                                jamieToJohnMessages: ["Hi, John", "How are you doing?", "Awesome, I'm good too"],
+                                johnToJamieMessages: ["Hey Jamie", "I'm good thanks, how are you?"],
+                            }
+                                
+                    
+                            
+                         
+});
 }
 
 module.exports.profile = function(req, res){
