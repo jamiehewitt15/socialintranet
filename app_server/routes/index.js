7e8d1b6b23c15791f81b1554c49f5da03f826603
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-01-14 15:24:52 +0000

    splitting up serverside controllers

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,19 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 var action = require("../controllers/main")
+var newsController = require("../controllers/news")
 
 /* news */
-router.get('/', action.newsList);
-router.post('/:newsid/newsLikes', action.doNewsLikes);
+router.get('/', newsController.newsList);
+router.post('/:newsid/newsLikes', newsController.doNewsLikes);
 
-router.get('/createproject', action.createProject);
-router.get('/project', action.project);
-router.get('/discover', action.discover);
+/* questions */
 router.get('/qa', action.qa);
-router.get('/chat', action.chat);
-router.get('/profile', action.profile);
+
+/* users */
 router.get('/newuser', action.newUser);
 router.post('/newuser', action.doNewUser);
 
+/* other */
+router.get('/createproject', action.createProject);
+router.get('/project', action.project);
+router.get('/discover', action.discover);
+router.get('/chat', action.chat);
+router.get('/profile', action.profile);
 
 module.exports = router;
