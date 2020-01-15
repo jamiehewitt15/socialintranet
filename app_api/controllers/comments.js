89fc1576c8163163dde4f2ac137b7de2a0269f1e
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-01-15 14:12:54 +0000

    working comment system on the front end

--- app_api/controllers/comments.js
+++ app_api/controllers/comments.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var News = mongoose.model('news');
+var Comments = mongoose.model('news');
 
 var sendJsonResponse = function (res, status, content){
     res.status(status);
@@ -7,6 +7,7 @@ var sendJsonResponse = function (res, status, content){
 }
 
 module.exports.newsCommentCreate = function (req, res) {
+    console.log("API newsCommentCreate START")
     var newsid = req.params.newsid
     if(newsid){
         Comments.findById(newsid)
@@ -16,9 +17,28 @@ module.exports.newsCommentCreate = function (req, res) {
                             sendJsonResponse(res, 400, err);
                         }else{
                             doAddComment(req, res, news);
+                            console.log("API newsCommentCreate SUCCESS")
                         }
                     });
     } else {
         sendJsonResponse(res, 404, {"message" : "News id not found."});
     }
 };
+
+var doAddComment = function(req, res, news){
+    console.log(req.body.name);
+    console.log(req.body.comment);
+    news.comments.push({
+        name: req.body.name,
+        comment: req.body.comment
+    });
+    news.save( function(err, news){
+        var thisComment;
+        if(err){
+            sendJsonResponse(res, 400, err);
+        }else{
+            thisComment = news.comments[news.comments.length - 1];
+            sendJsonResponse(res, 201, thisComment);
+        }
+    });
+}
