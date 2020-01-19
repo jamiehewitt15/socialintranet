ede8d22da1078cbed76ad8266bc84d6c634b738f
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-01-19 19:42:25 +0000

    adding angular controller for users page

--- public/angular/intranetApp.js
+++ public/angular/intranetApp.js
@@ -1,1 +1,24 @@
-angular.module('Intranet', [])
+angular.module('userListController', [])
+
+var usersList = function ($scope){
+    $scope.data - {
+        users: [{
+            name: "Jamie Hewitt",
+            location: "UK"
+        }, {
+            name: "Rachel Wanyoike",
+            location: "Kenya"
+        }
+    ]
+    }
+}
+
+var myController = function($scope)
+{
+  $scope.items = ["one", "two", "three"];
+};
+
+console.log("Angular Accessed")
+angular
+    .module('intranetApp')
+    .controller('userListController', 'userListController')
