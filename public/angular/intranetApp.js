1a6e469185fb99909715bfd2e9ffe0c87c5198c1
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-01-20 17:57:01 +0300

    angular working on users page view

--- public/angular/intranetApp.js
+++ public/angular/intranetApp.js
@@ -1,14 +1,36 @@
-angular.module('userListController', [])
+angular.module('intranetApp', [])
 
 var usersList = function ($scope){
     $scope.data = {
         users: [{
             name: "Jamie Hewitt",
-            location: "UK"
+            location: "UK",
+            joined: "2019-12-15T12:17:52.242Z",
         }, {
             name: "Rachel Wanyoike",
-            location: "Kenya"
+            location: "Kenya",
+            joined: "2020-01-05T12:17:52.242Z",
+        },
+        {
+            name: "John Roberts",
+            location: "USA",
+            joined: "2020-01-01T12:17:52.242Z",
+        },
+        {
+            name: "Alec Hewitt",
+            location: "UK",
+            joined: "2020-01-15T12:17:52.242Z",
+        }, {
+            name: "Kui Wanyoike",
+            location: "Kenya",
+            joined: "2020-01-20T12:17:52.242Z",
+        },
+        {
+            name: "George Roberts",
+            location: "USA",
+            joined: "2020-01-11T12:17:52.242Z",
         }
+
     ]
     }
 }
@@ -17,4 +39,4 @@ var usersList = function ($scope){
 console.log("Angular Accessed")
 angular
     .module('intranetApp')
-    .controller('usersList', 'usersList')
+    .controller('usersList', usersList)
